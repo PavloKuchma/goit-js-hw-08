2f1e02800c2e7dfa411846e40f6d2cf7808b429c
PavloKuchma <111251902+PavloKuchma@users.noreply.github.com>
2023-09-08 23:14:10 +0300

    task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+function renderGallery(arr) {
+  const renderList = arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join('');
+  listEl.insertAdjacentHTML('beforeend', renderList);
+}
+renderGallery(galleryItems);
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
