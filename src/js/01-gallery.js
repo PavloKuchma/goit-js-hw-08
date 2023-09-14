5977b362258c103e19f87157c3d02e3f34509424
PavloKuchma <111251902+PavloKuchma@users.noreply.github.com>
2023-09-14 21:50:11 +0300

    task 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -12,7 +12,7 @@ const listEl = document.querySelector('.gallery');
 function renderGallery(arr) {
   const renderList = arr
     .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
+      return `<li class="gallery__item" style="list-style: none">
       <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(VIDEO_CURRENT_TIME, JSON.stringify(data.seconds));
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(VIDEO_CURRENT_TIME))
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
